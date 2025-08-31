d64502c4fec291606b7ee6992a71b885ef791599
shopvixhelp-debug <shopvixhelp@gmail.com>
2025-08-31 14:36:17 +0530

    Updated app-build-manifest.json

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "4d768cbfac4fea75"; }
+/******/ 		__webpack_require__.h = function() { return "0b1e401b96e42c3f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
